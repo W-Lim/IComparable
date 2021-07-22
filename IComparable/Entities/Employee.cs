2f6d193d973064d3dd3ec61dc642052f1bf9a382
Wender lima <wenderlima678@gmail.com>
2021-07-22 13:58:40 -0300

    Codigo sobre ICoparable

---
+++ IComparable/Entities/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace IComparablee.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEMployee)
+        {
+            string[] vect = csvEMployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Salary: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is nor an employee");
+            }
+            Employee other = obj as Employee;
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
